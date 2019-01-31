correct result from both boson and fermion calculation. next step need to tune the parameter for the final results.

BSEquation.py
@@ -249,11 +249,11 @@ def compute_ixbands(file_dir, seed_name):
   #print(np.amin(ix_band_2))
   ix_band_1 -= ground_state
   ix_band_2 -= ground_state
-  print('band calulation completed')
+  print('band calulation completed: ground state energy = 0')
   return ix_band_1, ix_band_2
 
 # main program
-def main():
+def unit_test():
   '''main'''
   dir_mos2 = '/Users/wk/Dropbox/2015rp/TMDC_Exciton/Tight-binding Model/MoS2/'
   seedname = 'MoS2'
@@ -295,4 +295,4 @@ def main():
   print('end plot')
 
 if __name__ == "__main__":
-  main()
+  unit_test()

IXTE.py
@@ -5,14 +5,15 @@ import matplotlib
 import matplotlib.pyplot as plt
 import BSEquation as BSE
 from tight_binding import band_fitting
-from transport_module import EnergyVelocity, Statistics, correlation
+from transport_module import TightBindingModel, Statistics, correlation
 from transport_module import thermal_result
-from const import pi, KNUMB, EVALUATION_POINTS, TEMP, NT
+from const import pi, KNUMB, EVALUATION_POINTS, TEMP, NT, KNUMB_TRANS, NT_TRANS
 matplotlib.use('TkAgg')
 
-def generate_k_grid():
+grid_number = KNUMB_TRANS
+def generate_k_grid(grid_number):
   '''Generate the meshgrid'''
-  lines = np.linspace(0, 2 * pi, KNUMB) 
+  lines = np.linspace(0, 2 * pi, grid_number)
   #not necessary to be KNUMB
   k_x, k_y = np.meshgrid(lines, lines)
   return [k_x, k_y]
@@ -29,31 +30,39 @@ def main():
   t_2 = band_fitting(ix_band_2)
   np.savetxt("t_1.dat", t_1)
   np.savetxt("t_2.dat", t_2)
+  print("t_1: ", t_1)
+  print("t_2: ", t_2)
+  print("tight binding model completed.")
+  print("start to calculate the transportations")
 
-  k_grid = generate_k_grid()
-  band_1 = EnergyVelocity(k_grid, t_1)
-  band_2 = EnergyVelocity(k_grid, t_2)
+  k_grid = generate_k_grid(grid_number)
+  band_1 = TightBindingModel(k_grid, t_1)
+  band_2 = TightBindingModel(k_grid, t_2)
   ex_band_1, ex_velocity_1 = band_1.get_results()
   ex_band_2, ex_velocity_2 = band_2.get_results()
   print(ex_band_1.shape)
 # Prepare evaluation points for the thermal coefficient:
-  fugacity = list(map(lambda x: 1.0 * (1.0 + x) / 100, range(EVALUATION_POINTS)))
-  mu_points = list(map(lambda x: TEMP * np.log(x), fugacity))  # -np.log(fugacity)*temp
+  fugacity = list(map(lambda x: 2.0 * (1.0 + x), range(EVALUATION_POINTS)))
+  mu_points = list(map(lambda x: -TEMP * np.log(x), fugacity))  # -np.log(fugacity)*temp
   print('mu_points:', mu_points)
   ex_L0 = []
   ex_L1 = []
   ex_L2 = []
   ex_density = []
   for mu_ in mu_points:
+    print("mu: ", mu_)
     ex_n_1, ex_dn_1 = Statistics.bose_(ex_band_1, mu_, TEMP)
     ex_n_2, ex_dn_2 = Statistics.bose_(ex_band_2, mu_, TEMP)
-    density = (sum(ex_n_1) + sum(ex_n_2)) / NT
+    density = (ex_n_1.sum()) / NT_TRANS
+    print("density: ", density)
     eng_mu_1 = ex_band_1 - mu_
     eng_mu_2 = ex_band_2 - mu_
 
     ex_l0_1, ex_l1_1, ex_l2_1 = correlation(eng_mu_1, ex_velocity_1, ex_dn_1)
     ex_l0_2, ex_l1_2, ex_l2_2 = correlation(eng_mu_2, ex_velocity_2, ex_dn_2)
-    print('ex_l0', ex_l0_1)
+    print('ex_l0_1, ex_l0_2:', ex_l0_1)
+    print('ex_l1_1, ex_l1_2:', ex_l1_1)
+    print('ex_l2_1, ex_l2_2:', ex_l2_1)
     ex_L0.append(ex_l0_1 + ex_l0_2)
     ex_L1.append(ex_l1_1 + ex_l1_2)
     ex_L2.append(ex_l2_1 + ex_l2_2)
@@ -63,9 +72,13 @@ def main():
   results = map(thermal_result, ex_L0, ex_L1, ex_L2)
 
   [Lb, conduct, seebeck, thermal_conduct, zT, PF] = list(zip(*results))[:6]
-  print(Lb)
+  print("Lb: ", Lb)
+  print("zT: ", zT)
+  print("seebeck: ", seebeck)
+  print("conduct: ", conduct)
+  print("thermal: ", thermal_conduct)
   print("compute finished")
-
+'''
   plt.figure(1)
   plt.title('Bose Lorenz number')
   plt.xlabel('density(per unit cell)')
@@ -98,7 +111,7 @@ def main():
   ax2.semilogx(ex_density, PF, 'b-')
   ax2.set_ylabel('PF')
   plt.show()
-
+'''
 
 if __name__ == "__main__":
   main()

const.py
@@ -3,10 +3,12 @@ import math
 import numpy as np
 
 GS = 4.0
-KAPPA_PH = 19.  # 34.0
+KAPPA_PH = 34.0
 TEMP = 0.026  # temperature in EV
 KNUMB = 6
+KNUMB_TRANS = 144
 NT = KNUMB * KNUMB
+NT_TRANS = KNUMB_TRANS * KNUMB_TRANS
 
 pi = math.pi
 V = 0.1 / NT
@@ -15,17 +17,17 @@ HBAR = 1.05e-34     #Planck constant
 EV = 1.6e-19        #electron unit
 KB = 1.38e-23       #Boltzmann constant
 
-A_LAT = 3.16e-10    #a-axis of MoS2
-C_LAT = 12.30e-10   #c-axis of MoS2
-UNIT_CELL = A_LAT ** 2.0 * C_LAT * np.sqrt(3.0) / 2.0
-R_TAU = 1.05e-14 * 43.6 * 5.4  # electron*racial
+A_LAT = 3.16e-10    #a-axis of MoS2, unit m
+C_LAT = 12.30e-10   #c-axis of MoS2, unit m 
+UNIT_CELL = A_LAT ** 2 * C_LAT * np.sqrt(3.0) / 2.0
+R_TAU = 1.05e-14 * 43.6  # electron*racial
 VELOCITY_UNIT = EV * A_LAT / HBAR / (np.sqrt(3.0) / 2.0)
     # eV is for switch EV to J ?
 
 CONDUCT_UNIT = (VELOCITY_UNIT ** 2 * R_TAU /
-                UNIT_CELL / EV * EV ** 2) / NT / 1.e6
-SEEBECK_UNIT = KB / EV / TEMP * 1.e6
-THERMAL_CONDUCT_UNIT = (VELOCITY_UNIT ** 2 * R_TAU / UNIT_CELL * KB) / TEMP / NT
+                UNIT_CELL / EV * EV ** 2) / NT_TRANS
+SEEBECK_UNIT = KB / EV / TEMP
+THERMAL_CONDUCT_UNIT = (VELOCITY_UNIT ** 2 * R_TAU / UNIT_CELL * KB) / TEMP / NT_TRANS
 # K-Grid for calculation
 K_GRID = [[float(k_x) / KNUMB, float(k_y) / KNUMB] for k_x in range(KNUMB)
           for k_y in range(KNUMB)]

tight_binding.py
@@ -16,18 +16,19 @@ cos = np.cos
 def band_expansion(k, *t):
   '''hopping terms'''
   x, y = k
-  f_test = t[0]+2.0*t[1]*(cos(2.0*pi*x)+cos(2.0*pi*y)+cos(2.0*pi*(x+y)))\
-    + 2.0*t[2]*(cos(2.0*pi*2.0*x)+cos(2.0*pi*2.0*y)+cos(2.0*pi*2.0*(x+y)))\
-    + 2.0*t[3]*(cos(2.0*pi*(x+2.0*y))+cos(2.0*pi*(2.0*x+y))+cos(2.0*pi*(x-y))) \
-    + 2.0*t[4]*(cos(2.0*pi*(2.0*x+4.0*y))
-                + cos(2.0*pi*(2.0*y-4.0*(x+y)))+cos(2.0*pi*(-2.0*(x+y)+4.0*x)))
+  f_test = t[0] \
+    + 2.0*t[1]*(cos(x)+cos(y)+cos(x+y))\
+    + 2.0*t[2]*(cos(2.0*x)+cos(2.0*y)+cos(2.0*(x+y)))\
+    + 2.0*t[3]*(cos((x+2.0*y))+cos((2.0*x+y))+cos((x-y))) \
+    + 2.0*t[4]*(cos(2.0*(x+2.0*y))
+                + cos(2.0*(y-2.0*(x+y)))+cos(2.0*(-(x+y)+2.0*x)))
   return f_test
 
 # def band_terms(k_points):
 k_list = []
 for j in range(KNUMB):
   for i in range(KNUMB):
-    k_list.append([float(i)/KNUMB, float(j)/KNUMB])
+    k_list.append([2.0 * pi * float(i)/KNUMB, 2.0 * pi * float(j)/KNUMB])
 klistT = np.transpose(k_list)
 
 def band_fitting(data_source):

transport_module.py
@@ -13,31 +13,35 @@ from const import * #pi, KNUMB, GS, CONDUCT_UNIT, SEEBECK_UNIT, THERMAL_CONDUCT_
 cos = np.cos
 sin = np.sin
 
-class EnergyVelocity():  # need to improve to mesh_grid computation
+class TightBindingModel():  # need to improve to mesh_grid computation
   ''' exciton band structure '''
   def __init__(self, k_grid, t):
     self.k_grid = k_grid
     self.t = t
 
   def cal_energy(self):  # take meshgrid as input
-    kx = self.k_grid[0]
-    ky = self.k_grid[1]
+    kx, ky = self.k_grid
     term0 = np.full(kx.shape, 1.0)
     term1 = 2.0 * (cos(kx) + cos(ky) + cos(kx + ky))
     term2 = 2.0 * (cos(2.0 * kx) + cos(2.0 * ky) + cos(2.0 * (kx + ky)))
     term3 = 2.0 * (cos(kx + 2.0 * ky) + cos(2.0 * kx + ky) + cos(kx - ky))
-    term4 = 2.0 * (cos(2.0 * kx + 4.0 * ky) + cos(2.0 * ky - 4.0 * (kx + ky)) + cos(2.0 * (kx - ky)))
+    term4 = 2.0 * (cos(2.0 * kx + 4.0 * ky) + cos(2.0 * ky - 4.0 * (kx + ky))
+                   + cos(2.0 * (kx - ky)))
     hopping_terms = [term0, term1, term2, term3, term4]
     return sum(map(lambda x, y: x * y, self.t, hopping_terms))
 
+  '''
+  In the long wavelength limit, the transport is almost isotropic.
+  We can take transport along x-direction without lossing generacity.
+  v_x = dE / dkx
+  '''
   def cal_velocity(self):
-    kx = self.k_grid[0]
-    ky = self.k_grid[1]
+    kx, ky = self.k_grid
     term0 = np.zeros(kx.shape)
-    term1 = 2.0 * (sin(kx) + 0.5 * sin(kx + ky) - 0.5 * sin(ky))
-    term2 = 2.0 * (sin(2.0 * kx) + 0.5 * (sin(2.0 * (kx + ky)) - sin(2.0 * ky)))
-    term3 = 2.0 * (sin(2.0 * kx + ky) + sin(kx - ky))
-    term4 = 2.0 * (sin(4.0 * kx + 2.0 * ky) + sin(2.0 * (kx - ky)))
+    term1 = -2.0 * (sin(kx) + 0.5 * sin(kx + ky) - 0.5 * sin(ky))
+    term2 = -2.0 * (2.0 * sin(2.0 * kx) + sin(2.0 * (kx + ky)) - sin(2.0 * ky))
+    term3 = -3.0 * (sin(2.0 * kx + ky) + sin(kx - ky))
+    term4 = 3.0 * (sin(-4.0*kx-2.0*ky) - sin(2.0*(kx - ky)))
     velocity_terms = [term0, term1, term2, term3, term4]
     return sum(map(lambda x, y: x * y, self.t, velocity_terms))
 
@@ -65,18 +69,17 @@ def correlation(eng_mu, velocity, d_density):
   l0_grid = velocity * velocity * d_density
   l1_grid = eng_mu * l0_grid
   l2_grid = eng_mu * l1_grid
-  l0 = sum(sum(l0_grid)) #double check thisi
-  print("l0: ", l0.size)
-  l1 = sum(sum(l1_grid))
-  l2 = sum(sum(l2_grid))
+  l0 = l0_grid.sum()
+  l1 = l1_grid.sum()
+  l2 = l2_grid.sum()
   return l0, l1, l2
 
 def thermal_result(l0_, l1_, l2_):
-  Lb = (l0_ * l0_ - l1_ * l1_) / (l0_ * l0_ * TEMP ** 2)
+  Lb = (l2_ * l0_ - l1_ * l1_) / (l0_ * l0_ * TEMP ** 2)
   conduct = l0_ * CONDUCT_UNIT
   seebeck = l1_ / l0_ * SEEBECK_UNIT
   thermal_conduct = (l2_ - (l1_ * l1_) / l0_) * THERMAL_CONDUCT_UNIT
   zT = conduct * seebeck ** 2 * TEMP / \
   (thermal_conduct + 2.0 * KAPPA_PH) * EV / KB
-  PF = conduct * seebeck ** 2 * 1.e3
+  PF = conduct * seebeck ** 2
   return [Lb, conduct, seebeck, thermal_conduct, zT, PF]